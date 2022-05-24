0f2626bf27229a380d3c5900acb8a5daa7b2873f
Roberto Azeredo <robertoazdd@gmail.com>
2022-05-23 21:49:21 -0300

    Adicionado gráfico de navegação com destino inicial ao HomeFragment

--- app/src/main/java/com/robertoazeredo/nytimes/ui/main/MainActivity.kt
+++ app/src/main/java/com/robertoazeredo/nytimes/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.robertoazeredo.nytimes
+package com.robertoazeredo.nytimes.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.robertoazeredo.nytimes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
