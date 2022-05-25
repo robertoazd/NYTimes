27713edcf9e8284b637b3369ac77a4255d28dbca
Roberto Azeredo <robertoazdd@gmail.com>
2022-05-25 09:39:46 -0300

    Adicionado viewBinding, layout do HomeFragment e alteração no colors

--- app/src/main/java/com/robertoazeredo/nytimes/ui/view/HomeFragment.kt
+++ app/src/main/java/com/robertoazeredo/nytimes/ui/view/HomeFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.robertoazeredo.nytimes.R
+import com.robertoazeredo.nytimes.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.buttonSections.setOnClickListener {
+            println("CLICK")
+        }
     }
 }
