41e2de85563d55dee436bfeffd945b6bb29c639f
Roberto Azeredo <robertoazdd@gmail.com>
2022-05-25 21:56:00 -0300

    Adicionado layout do SectionsFragment

--- app/src/main/java/com/robertoazeredo/nytimes/ui/view/SectionsFragment.kt
+++ app/src/main/java/com/robertoazeredo/nytimes/ui/view/SectionsFragment.kt
@@ -5,15 +5,64 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.robertoazeredo.nytimes.R
+import androidx.navigation.fragment.findNavController
+import com.robertoazeredo.nytimes.databinding.FragmentSectionsBinding
 
 class SectionsFragment : Fragment() {
 
+    private lateinit var binding: FragmentSectionsBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sections, container, false)
+    ): View {
+        binding = FragmentSectionsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.buttonWorld.setOnClickListener {
+            println("World")
+        }
+        binding.buttonUs.setOnClickListener {
+            println("Us")
+        }
+        binding.buttonPolitics.setOnClickListener {
+            println("Politics")
+        }
+        binding.buttonNy.setOnClickListener {
+            println("Ny")
+        }
+        binding.buttonBusiness.setOnClickListener {
+            println("Business")
+        }
+        binding.buttonTech.setOnClickListener {
+            println("Tech")
+        }
+        binding.buttonScience.setOnClickListener {
+            println("Science")
+        }
+        binding.buttonSports.setOnClickListener {
+            println("Sports")
+        }
+        binding.buttonBooks.setOnClickListener {
+            println("Books")
+        }
+        binding.buttonFood.setOnClickListener {
+            println("Food")
+        }
+        binding.buttonTravel.setOnClickListener {
+            println("Travel")
+        }
     }
 }
+
