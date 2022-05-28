7886d66f45f4d61f90916e8ccfc8b893d8afcb7e
Roberto Azeredo <robertoazdd@gmail.com>
2022-05-27 21:11:56 -0300

    Adicionado NewsRepository e testado função getNews com seção hardcode

---
+++ app/src/main/java/com/robertoazeredo/nytimes/data/repository/NewsRepository.kt
@@ -0,0 +1,9 @@
+package com.robertoazeredo.nytimes.data.repository
+
+import com.robertoazeredo.nytimes.data.api.ApiService
+import retrofit2.Call
+
+class NewsRepository {
+
+    fun getNews(): Call<String> = ApiService.newsApi.getNews()
+}
--- app/src/main/java/com/robertoazeredo/nytimes/ui/view/ArticlesFragment.kt
+++ app/src/main/java/com/robertoazeredo/nytimes/ui/view/ArticlesFragment.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.robertoazeredo.nytimes.data.repository.NewsRepository
 import com.robertoazeredo.nytimes.databinding.FragmentArticlesBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ArticlesFragment : Fragment() {
 
@@ -25,9 +29,22 @@ class ArticlesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupView()
+        getNews()
     }
 
     private fun setupView() {
         binding.textSection.text = args.section
+    }
+
+    private fun getNews() {
+        NewsRepository().getNews().enqueue(object : Callback<String>{
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                println(response)
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                println(t)
+            }
+        })
     }
 }
