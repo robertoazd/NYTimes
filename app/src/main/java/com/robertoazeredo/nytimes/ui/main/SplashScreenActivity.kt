8f9a70a3235045a76123c7285351959d7efa5319
Roberto Azeredo <robertoazdd@gmail.com>
2022-07-19 23:31:53 -0300

    Adicionado alterações no tema

--- app/src/main/java/com/robertoazeredo/nytimes/ui/main/SplashScreenActivity.kt
+++ app/src/main/java/com/robertoazeredo/nytimes/ui/main/SplashScreenActivity.kt
@@ -1,16 +1,19 @@
 package com.robertoazeredo.nytimes.ui.main
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
 import com.robertoazeredo.nytimes.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        setupSavedTheme()
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -22,5 +25,16 @@ class SplashScreenActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }, 2000)
+    }
+
+    private fun setupSavedTheme() {
+        val sharedPreferences = getSharedPreferences("SharedPrefs", Context.MODE_PRIVATE)
+        val modeNightEnabled = sharedPreferences.getBoolean("MODE_NIGHT", false)
+
+        if (modeNightEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
