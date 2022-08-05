35a65909dff0c0e779a4a42db988e6d2456ff3c5
Roberto Azeredo <robertoazdd@gmail.com>
2022-08-05 15:15:56 -0300

    Adicionado configurações da versão 1.0.0

--- app/src/main/java/com/robertoazeredo/nytimes/data/api/NewsApi.kt
+++ app/src/main/java/com/robertoazeredo/nytimes/data/api/NewsApi.kt
@@ -1,5 +1,6 @@
 package com.robertoazeredo.nytimes.data.api
 
+import com.robertoazeredo.nytimes.BuildConfig
 import com.robertoazeredo.nytimes.data.model.NewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -11,6 +12,6 @@ interface NewsApi {
     @GET("all/{section}.json")
     suspend fun getNews(
         @Path("section") section: String,
-        @Query("api-key") apiKey: String = "HAvkXB7TKnNJNoOw3ZDzADMfT2pmJal8"
+        @Query("api-key") apiKey: String = BuildConfig.API_KEY
     ): Response<NewsResponse>
 }
